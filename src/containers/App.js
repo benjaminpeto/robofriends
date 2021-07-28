ca1c159519bcc517acb604b54d4b7004f4ab178d
Benji <benjaminpeto23@gmail.com>
2021-07-28 16:45:44 +0100

    added redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,30 @@
 import React, { useState, useEffect } from "react";
+import { connect } from 'react-redux';
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
 import "./App.css";
+import { setSearchField, requestRobots } from '../actions';
+
+const mapStateToProps = state => {
+  return {
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => requestRobots(dispatch)
+  }
+}
 
 function App() {
   const [robots, setRobots] = useState([]);
-  const [searchfield, setSearchfield] = useState("");
+  const [searchField, setSearchField] = useState("");
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
@@ -16,11 +34,11 @@ function App() {
   //second argument is an array, so it won't run an infinite loop, you can declare inside the array, when the you want to fetch again
 
   const onSearchChange = (event) => {
-    setSearchfield(event.target.value);
+    setSearchField(event.target.value);
   };
 
   const filteredRobot = robots.filter((robot) => {
-    return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    return robot.name.toLowerCase().includes(searchField.toLowerCase());
   });
   return !robots.length ? (
     <h1 className="tc pt5">Loading...</h1>
@@ -34,4 +52,5 @@ function App() {
     </div>
   );
 }
-export default App;
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
